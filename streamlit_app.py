21868aedcc900bad008e3666af072c5c31d1ceb9
wbrown-snow <155670308+wbrown-snow@users.noreply.github.com>
2024-01-06 09:09:24 -0500

    Update streamlit_app.py
    add 2nd textbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,6 @@ my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The Fruit Load List contains:")
 streamlit.dataframe(my_data_row)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding: ', add_my_fruit)
