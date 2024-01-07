4159a0a0508325c05ae643b456180c801f180f9b
wbrown-snow <155670308+wbrown-snow@users.noreply.github.com>
2024-01-07 09:52:09 -0500

    Update streamlit_app.py
    add insert function

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,14 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.header("The Fruit Load List contains:")
   streamlit.dataframe(my_data_rows)
 
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list Values('from streamlit')")
+    return "Thanks for adding: " + new_fruit
+    
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('Thanks for adding: ', add_my_fruit)
-
-streamlit.stop()
-my_cur.execute("Insert into pc_rivery_db.public.fruit_load_list Values('from streamlit')")
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function =  insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+  
